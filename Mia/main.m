49a1e30b14c1605be3e5623d94cc05a8271374f3
Alioune Ndoye <alioune29@hotmail.com>
2013-07-14 12:41:16 +0000

    Initial Commit

---
+++ Mia/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mia
+//
+//  Created by Alioune Badara Ndoye on 14/07/13.
+//  Copyright Xtreme Design and Engineering 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
